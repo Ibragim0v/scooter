468b311cb73f6f1e8d8a851159aeb3f5b8c976a6
Ibragim0v <ibragimov.amirshoh@mail.ru>
2022-02-06 13:49:59 +0500

    Second commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 var burgerBtn = document.querySelector(".header__burger")
-var menu = document.querySelector("header")
+var elHeader = document.querySelector("header")
 
 burgerBtn.addEventListener("click", (evt) => {
-    menu.classList.toggle("header--open")
+    elHeader.classList.toggle("header--open")
 })
-
-// JAVASCRIPT
